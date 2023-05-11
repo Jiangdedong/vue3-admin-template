45131d882fa11feabd870c32fadbbd8b0f4e15cb
尾巴钓鱼的喵 <1308242265@qq.com>
2023-05-11 17:41:21 +0800

    Create index.vue
    补充redirect页面

---
+++ src/views/redirect/index.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { useRouter, useRoute } from 'vue-router'
+
+const $router = useRouter()
+const $route = useRoute()
+const { params, query } = $route
+const { path } = params
+$router.replace({ path: '/' + path, query })
+
+</script>
